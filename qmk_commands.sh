bb3bdcab2801f65404dbb5c44e866a3c74634517
Guilherme Parreira <guilhermeparreira.silva@gmail.com>
2025-05-20 23:02:18 -0300

    first keymaps

--- qmk_commands.sh
+++ qmk_commands.sh
@@ -9,13 +9,35 @@ pip3 install qmk
 `qmk list-keyboard`
 # sofle/rev1 is mine
 
-# Create a new keymap
-qmk new-keymap -kb sofle/rev2 -km personal
 
-qmk compile -kb sofle/rev1 -km personal
-qmk flash -kb sofle/rev1 -km personal
+# It will builds the keyboard's default keymap, which -kb is /home/guilherme/qmk_firmware/keyboards/sofle/keymaps/default
+qmk compile -kb sofle/rev1 -km default
 
- 
+############
+# Step to build my own env
+# keymap.c is the REAL keymap file that arduio understand
+############
+# First, config
+qmk config user.keyboard=sofle/rev1
+qmk config user.keymap=guilherme
+
+####### Didn't use
+# You can convert this file to a keymap.c using this command: 
+qmk json2c sofle_rev1_layout_2025-05-20.json # And then the result of it, I would need to put in the folder
+# Create a new keymap (I think)
+qmk new-keymap -kb sofle/rev1 -km guilherme
+
+####### I used.
+# You can compile this keymap using this command: 
+qmk compile sofle_rev1_layout_2025-05-20.json
+
+qmk flash
+
+coração
 # Flashing after changing the layout in qmk online
 cp ~/Downloads/sofle_rev1_layout_2025-05-19.hex ~/qmk_firmware/.build/sofle_rev1_default.hex
 qmk flash -kb sofle/rev1 -km personal
+
+# Command to flash
+qmk flash -bl dfu -f ~/Downloads/sofle_rev1_layout_2025-05-20.hex
+qmk flash -kb sofle/rev1 -bl dfu -f ~/Downloads/sofle_rev1_layout_2025-05-20.hex

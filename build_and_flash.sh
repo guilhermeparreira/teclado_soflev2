5b83d53f1880684e33641492c43d3b06b63939df
Guilherme Parreira <guilhermeparreira.silva@gmail.com>
2025-06-23 09:42:27 -0300

    new version of my keyboard

--- build_and_flash.sh
+++ build_and_flash.sh
@@ -6,6 +6,7 @@ qmk flash -kb sofle/rev1 -km "$(pwd)"
 
 
 ## It works
+cp -f ~/Downloads/v2_2025-05-20.json ~/Documents/teclado_sofle_v2.json # To save in git only
 qmk json2c ~/Downloads/v2_2025-05-20.json -o ~/qmk_firmware/keyboards/sofle/keymaps/personal/keymap.c
 cd ~/qmk_firmware
 qmk flash -kb sofle/rev1 -km personal
